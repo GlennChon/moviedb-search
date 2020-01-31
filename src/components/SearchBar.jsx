05eb18b8bff9e3d9a5241a4befe3439896ddac59
glennchon <glenn.chon@gmail.com>
2020-01-31 21:21:11 +0700

    styling and onblur

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { InputGroup, Form, Button } from "react-bootstrap";
-
+import { Container, InputGroup, Form, Button } from "react-bootstrap";
+import "./searchbar.css";
 export const SearchBar = ({
   id,
   name,
   handleChange,
   handleSubmit,
   value,
-  placeholder
+  placeholder,
+  inputRef,
+  onBlur
 }) => {
   return (
     <Form onSubmit={handleSubmit}>
       <InputGroup>
         <Form.Control
+          className="input"
           id={id}
           name={name}
           placeholder={placeholder}
           value={value}
           aria-label="search bar"
           onChange={handleChange}
+          onBlur={onBlur}
+          ref={inputRef}
         />
         <InputGroup.Append>
           <Button onClick={handleSubmit}>Search</Button>
