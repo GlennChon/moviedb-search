b0a8ff5d40d6f51db70ce20197189948e29f29ac
glennchon <glenn.chon@gmail.com>
2020-01-31 21:22:02 +0700

    displays movie cards

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,3 +1,19 @@
 import React from "react";
+import { MovieCard } from "./MovieCard";
+import { Row, Col } from "react-bootstrap";
 
-export const SearchResults = () => {};
+export const SearchResults = ({ results, baseImgPath }) => {
+  return (
+    <Row>
+      {results.map((movie, i) => (
+        <Col>
+          <MovieCard
+            key={i}
+            title={movie.title}
+            imgPath={baseImgPath + movie.poster_path}
+          />
+        </Col>
+      ))}
+    </Row>
+  );
+};
