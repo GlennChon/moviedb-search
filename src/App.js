48007879c1bd7ca17f1ed8bd742bf977e418ecd0
glennchon <glenn.chon@gmail.com>
2020-01-31 13:55:34 +0700

    housekeeping and simple search test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getSearchResults } from "./services/moviedbService";
 function App() {
+  useEffect(() => {
+    const test = async () => {
+      const result = await getSearchResults("jack reacher");
+      console.log(result);
+    };
+    test();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Search Bar</div>
+      <div>
+        Body
+        <div>Results</div>
+      </div>
     </div>
   );
 }
