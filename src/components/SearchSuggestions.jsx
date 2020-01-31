3ba29173ffa21192728376f1bfb1d9db89898695
glennchon <glenn.chon@gmail.com>
2020-01-31 18:13:31 +0700

    refactor to listgroup

--- src/components/SearchSuggestions.jsx
+++ src/components/SearchSuggestions.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import Suggestion from "./Suggestion";
-import { Button } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
+import "./searchSuggestions.css";
 
-export const SearchSuggestions = ({ targetBaseUrl, values }) => {
-  const handleClick = () => {};
+const SearchSuggestions = ({ handleClick, baseImgPath, size, results }) => {
   return (
-    <React.Fragment>
-      {values.map((movie, i) => (
-        <Suggestion
+    <ListGroup className="list-group">
+      {results.slice(0, size).map((movie, i) => (
+        <ListGroup.Item
+          className="list-item"
           key={i}
-          title={movie.title}
-          onClick={handleClick(movie.id)}
-          imagePath={"https://image.tmdb.org/t/p/original" + movie.poster_path}
-          rating={movie.vote_average}
-          release={movie.release_date}
-          as={Button}
-        />
+          action
+          onClick={e => {
+            handleClick(e, movie);
+          }}
+        >
+          <Suggestion
+            title={movie.title}
+            imagePath={baseImgPath + movie.poster_path}
+            rating={movie.vote_average}
+            release={movie.release_date}
+          />
+        </ListGroup.Item>
       ))}
-    </React.Fragment>
+    </ListGroup>
   );
 };
+
+export default SearchSuggestions;
+
+/*
+
+      */
