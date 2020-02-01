066a2bceb4056fee50d1d666455d86b2055e99d3
glennchon <glenn.chon@gmail.com>
2020-02-01 19:35:49 +0700

    added search type and pages

--- src/services/moviedbService.js
+++ src/services/moviedbService.js
@@ -1,19 +1,28 @@
 import moviedb from "./config";
 // API key should technically be in an environment variable but for the sake of simplicity, it is kept in the config.js file imported above
 
-function searchUrl(searchValue) {
-  //https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
-  return `${moviedb.apiUrl}/search/movie?api_key=${moviedb.apiKey}&query=${searchValue}`;
+function searchUrl(searchValue, type, page) {
+  //https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher&page={page#}
+  return `${moviedb.apiUrl}/search/${type}?api_key=${
+    moviedb.apiKey
+  }&query=${searchValue}${
+    page !== null ? "&page=" + page : ""
+  }&include_adult=false`;
 }
 
 function movieDetailsUrl(movieID) {
   //api.themoviedb.org/3/movie/343611?api_key={api_key}
+
   return `${moviedb.apiUrl}/movie/${movieID}?api_key=${moviedb.apiKey}`;
 }
 
-export async function getSearchResults(searchValue) {
+export async function getSearchResults(
+  searchValue,
+  type = "movie",
+  page = null
+) {
   try {
-    let response = await fetch(searchUrl(searchValue));
+    let response = await fetch(searchUrl(searchValue, type, page));
     if (!response.ok) {
       throw response;
     } else {
