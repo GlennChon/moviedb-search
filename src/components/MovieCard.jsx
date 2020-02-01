03a2b2b603e2067a77530d015ae81def9c8c8638
glennchon <glenn.chon@gmail.com>
2020-02-01 19:33:50 +0700

    added movie object as prop

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Card } from "react-bootstrap";
 import "./moviecard.css";
 
-export const MovieCard = ({ imgPath, title }) => {
+export const MovieCard = ({ imgPath, movie, handleClick }) => {
   return (
-    <Card className="bg-dark movie-card">
-      <Card.Img src={imgPath} alt={`${title} Poster`} className="card-image" />
+    <Card
+      role="button"
+      name={movie.title + " card"}
+      className="btn bg-dark movie-card"
+      onClick={e => {
+        handleClick(e, movie);
+      }}
+    >
+      <Card.Img
+        src={imgPath}
+        alt={`${movie.title} Poster`}
+        className="card-image"
+      />
     </Card>
   );
 };
