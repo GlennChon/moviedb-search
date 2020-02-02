76f077b52928b4038519fea1e22f5487bbfe3c6a
glennchon <glenn.chon@gmail.com>
2020-02-02 14:46:29 +0700

    fade images in onload

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import "./moviecard.css";
 
 export const MovieCard = ({ imgPath, movie, handleClick }) => {
+  const [imgClassName, setImgClassName] = useState("card-image");
+  const [cardTextClassName, setCardTextClassName] = useState("");
+  const handleImgLoad = () => {
+    setCardTextClassName("hidden");
+    setImgClassName("card-image fade-in");
+    return;
+  };
   return (
     <Card
       role="button"
@@ -15,8 +22,12 @@ export const MovieCard = ({ imgPath, movie, handleClick }) => {
       <Card.Img
         src={imgPath}
         alt={`${movie.title} Poster`}
-        className="card-image"
+        className={imgClassName}
+        onLoad={handleImgLoad}
       />
+      <Card.Text className={cardTextClassName}>
+        <h1>{movie.title}</h1>
+      </Card.Text>
     </Card>
   );
 };
