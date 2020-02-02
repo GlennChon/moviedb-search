52a7a43954836ebc5d8d457de945feaf77f27683
glennchon <glenn.chon@gmail.com>
2020-02-02 14:46:47 +0700

    fixed overflow issues

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { MovieCard } from "./MovieCard";
-import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 //TODO: implement infinite scroll
 
 export const SearchResults = ({ results, baseImgPath, handleClick }) => {
+  const style = {
+    margin: "auto"
+  };
   return (
-    <Row className="justify-content-sm-center">
+    <>
       {results.map((movie, i) => (
-        <MovieCard
-          key={i}
-          movie={movie}
-          imgPath={baseImgPath + movie.poster_path}
-          handleClick={handleClick}
-        />
+        <Col xs="auto">
+          <MovieCard
+            style={style}
+            key={i}
+            movie={movie}
+            imgPath={baseImgPath + movie.poster_path}
+            handleClick={handleClick}
+          />
+        </Col>
       ))}
-    </Row>
+    </>
   );
 };
